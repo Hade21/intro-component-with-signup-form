27b43682d602bebab61f249b4fe1cec3b682df39
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-20 00:11:13 +0800

    add script.js

--- script.js
+++ script.js
@@ -10,22 +10,27 @@ function checkAll() {
     validasiPassword(pass);
     if (validasiNama == true && validasiEmail == true && validasiPassword == true) {
         return true;
+    } else {
+        return false;
     }
 }
 
-function validasiEmail() {
+function validasiEmail(event) {
     let mailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (email.value.match(mailCheck)) {
+    if (event.value.match(mailCheck)) {
         return true;
+    } else if (event.value == '') {
+        event.nextElementSibling.nextElementSibling.innerHTML = 'Please provide an email';
+        return false;
     } else {
-        email.nextElementSibling.nextElementSibling.style.display = 'block';
+        event.nextElementSibling.nextElementSibling.innerHTML = 'Looks like this is not an email';
         return false;
     }
 }
 
 function validasiNama(event) {
     if (event.value == '') {
-        event.nextElementSibling.nextElementSibling.style.display = 'block';
+        event.nextElementSibling.nextElementSibling.innerHTML = event.placeholder + ' cannot be empty';
         return false;
     } else {
         return true;
@@ -33,10 +38,14 @@ function validasiNama(event) {
 }
 
 function validasiPassword(event) {
-    let passCheck = /[A-Za-z0-9*@#$%&]/
-    if (event.value.length >= 8 && event.value == passCheck) {
+    let passCheck = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/
+    if (event.value == passCheck) {
         return true;
+    } else if (event.value == '') {
+        console.log(event.value)
+        event.nextElementSibling.nextElementSibling.innerHTML = pass.placeholder + ' cannot be empty';
+        return false;
     } else {
-        pass.nextElementSibling.nextElementSibling.style.display = 'block'
+        event.nextElementSibling.nextElementSibling.innerHTML = pass.placeholder + ' is not correct';
     }
 }
